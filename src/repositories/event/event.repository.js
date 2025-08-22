f9c3b2ee638c02d3673d9241a4021b7ab4d38d7b
unknown <iasminsena6013@digitalcollege.internal>
2025-08-21 21:30:14 -0300

    Criação da listagem unitaria

--- src/repositories/event/event.repository.js
+++ src/repositories/event/event.repository.js
@@ -1,9 +1,18 @@
 const {prisma} = require('../../config/database');
 
 async function find(take , skip ) {
-    return await prisma.event.findMany();
+        return await prisma.event.findMany();
+}
+
+async function findById(id) {
+    return await prisma.event.findUnique({
+        where: {
+            id
+        }
+    });
 }
 
 module.exports = {
-    find
+    find,
+    findById
 }
--- src/services/event/event.service.js
+++ src/services/event/event.service.js
@@ -11,8 +11,18 @@ async function find(take, skip) {
     console.log(take);
     console.log(skip);
     
-    return await eventRepository.find(take, skip);
+    return await eventRepository.find(take, skip); 
 }
+
+async function findById(id) {
+    
+    id === Number(id) ? id : new Error('oi')  
+    
+    return await eventRepository.findById(id);
+}
+
+
+
 
 module.exports = {
     find
