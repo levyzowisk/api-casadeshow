356f8fee9185331af9dbb41aa6dce220d234b42c
Iasmin Oliveira <iasminolisena@gmail.com>
2025-10-23 12:13:54 -0300

    Adiciona documentação Swagger inicial

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const example = require('./src/routes/example');
 const eventRoute = require('./src/routes/event/event.route');
 const userRoute = require('./src/routes/user/user.route');
 const authRoute = require('./src/routes/auth/auth.route');
-const artistRoute = require('./src/routes/artist/artist.route')
+const artistRoute = require('./src/routes/artist/artist.route');
 const handlerError = require('./src/middleware/error');
 const port = 3000;
 require('dotenv').config();
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs'); // 👈 Adiciona esta linha
+const swaggerDocument = YAML.load('./openapi.yaml'); // 👈 Carrega o arquivo YAML
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // 👈 Adiciona esta linha
 
 app.use('/api', example.router);
 app.use('/api/events', eventRoute.router);
@@ -19,7 +24,7 @@ app.use('/api/auths', authRoute.router);
 app.use('/api/artists', artistRoute.router);
 app.use(handlerError);
 
+
 app.listen(port, () => {
     console.log("App listening on port: " + port);
-    
-})
+});
